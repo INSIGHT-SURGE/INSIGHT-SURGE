52fb87c0c50430b40c8ad6dc5ad2d3bd9d44dc7d
Daneil-k <158211667+Daneil-k@users.noreply.github.com>
2024-03-27 10:19:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/INSIGHT-SURGE/INSIGHT-SURGE.git /INSIGHT-SURGE
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /INSIGHT-SURGE
 fi
-cd /TheMovieProviderBot
+cd /INSIGHT-SURGE
 pip3 install -U -r requirements.txt
 echo "Starting TheMovieProviderBot...."
 python3 bot.py
